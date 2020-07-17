1c603b6be2b8701e51798cef8f4975b7032579c1
JPSalado <jpsalado@orkestra.deusto.es>
2020-07-17 17:18:39 +0200

    ft: add first Bubble Chart
    Simple Bubble Chart with tooltip, opacity and text labels.

---
+++ gds_vizs/bubble_chart/src/index.js
@@ -0,0 +1,119 @@
+const d3 = require('d3');
+const dscc = require('@google/dscc');
+const local = require('./localMessage.js');
+
+export const LOCAL = true;
+
+const drawViz = (message) => {
+  const margin = {left: 100, right: 100, top: 100, bottom: 100};
+  const height = dscc.getHeight();
+  const width = dscc.getWidth();
+  const chartHeight = height - margin.top - margin.bottom;
+  const chartWidth = width - margin.left - margin.right;
+
+  // remove existing svgs
+  d3.select("body")
+      .selectAll("svg")
+      .remove();
+
+  // make a chartSvg
+  const svg = d3.select("body")
+      .append("svg")
+        .attr("width", chartWidth + margin.left + margin.right)
+        .attr("height", chartHeight + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add a scale for X
+  const xScale = d3.scaleLinear()
+      .domain([0, 20])
+      .range([0, chartWidth]);
+
+  // Add a scale for Y
+  const yScale = d3.scaleLinear()
+      .domain([0, 10])
+      .range([chartHeight, 0]);
+
+  // Add a scale for bubble size
+  const zScale = d3.scaleLinear()
+      .domain([0, 10])
+      .range([1, 50]);
+
+  // Add X axis
+  svg.append("g")
+      .attr("transform", "translate(0," + chartHeight + ")")
+      .call(d3.axisBottom(xScale));
+
+  // Add Y axis
+  svg.append("g")
+      .call(d3.axisLeft(yScale));
+
+  // Add tooltip
+    // -1- Create a tooltip div that is hidden by default:
+  const tooltip = d3.select("body")
+      .append("div")
+        .style("opacity", 1)
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("color", "white")
+
+  const showTooltip = function(d) {
+        tooltip
+            .transition()
+            .duration(200)
+        tooltip
+            .style("opacity", 1)
+            .html("TooltipX: " + d.xMetric + "<br />TooltipY: " + d.yMetric)
+            .style("left", d3.event.pageX+30 + "px")
+            .style("top", d3.event.pageY+30 + "px")
+    }
+  const moveTooltip = function(d) {
+        tooltip
+            .style("left", (d3.event.pageX+30) + "px")
+            .style("top", (d3.event.pageY+30) + "px")
+    }
+  const hideTooltip = function(d) {
+      tooltip
+          .transition()
+          .duration(200)
+            .style("opacity", 0)
+    }
+  // add dots
+  svg.append("g")
+      .selectAll("dot")
+      .data(message.tables.DEFAULT)
+      .enter()
+      .append("circle")
+        .attr("class", "bubbles")
+        .attr("cx", function (d) { return xScale(d.xMetric); } )
+        .attr("cy", function (d) { return yScale(d.yMetric); } )
+        .attr("r", function (d) { return zScale(d.sizeMetric); } )
+        .style("stroke", "black")
+        .style("opacity", "0.7")
+        .style("fill", "blue")
+        .on("mouseover", showTooltip )
+        .on("mousemove", moveTooltip )
+        .on("mouseleave", hideTooltip )
+        // .style("fill", function (d) { return myColor(d.continent); } )
+
+  svg.append("g")
+      .selectAll("text")
+      .data(message.tables.DEFAULT)
+      .enter()
+      .append("text")
+        .attr("x", function (d) {return xScale(d.xMetric); })
+        .attr("y", function (d) { return yScale(d.yMetric); })
+        .attr("text-anchor", "middle")
+        .text(function (d) { return d.dimension; });
+};
+
+// renders locally
+if (LOCAL) {
+  drawViz(local.message);
+} else {
+  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
+}
---
+++ gds_vizs/bubble_chart/src/localMessage.js
@@ -0,0 +1,275 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimension": [
+          "Pablo"
+        ],
+        "xMetric": [
+          20
+        ],
+        "yMetric": [
+          8
+        ],
+        "sizeMetric": [
+          7
+        ]
+      },
+      {
+        "dimension": [
+          "Eva"
+        ],
+        "xMetric": [
+          15
+        ],
+        "yMetric": [
+          10
+        ],
+        "sizeMetric": [
+          9
+        ]
+      },
+      {
+        "dimension": [
+          "Julian"
+        ],
+        "xMetric": [
+          5
+        ],
+        "yMetric": [
+          3
+        ],
+        "sizeMetric": [
+          6
+        ]
+      },
+      {
+        "dimension": [
+          "Romero"
+        ],
+        "xMetric": [
+          3
+        ],
+        "yMetric": [
+          4
+        ],
+        "sizeMetric": [
+          4
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "dimension": [
+      {
+        "id": "qt_rccgy1f79b",
+        "name": "student_name",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "xMetric": [
+      {
+        "id": "qt_nfbpa2f79b",
+        "name": "study_time",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ],
+    "yMetric": [
+      {
+        "id": "qt_mbh3c2f79b",
+        "name": "score",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ],
+    "sizeMetric": [
+      {
+        "id": "qt_bxiye2f79b",
+        "name": "avg_score_previous_exams",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {},
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {}
+};
